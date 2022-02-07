cadf2115421ecd12e96bdb79b60f6ef40c931d00
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-07 08:04:46 -0300

    Login e logout

---
+++ Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefas.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        public string User { get; set; }
+    }
+}
